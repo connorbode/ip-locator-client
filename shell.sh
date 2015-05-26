0dc9636be618fd8a224bf21d090fd3ef42d5016a
Connor Bode <connor.bode@gmail.com>
2015-05-26 18:56:44 -0400

    started checking supplied parameters

--- shell.sh
+++ shell.sh
@@ -3,3 +3,18 @@ command -v curl >/dev/null 2>&1 || {
   exit 1; 
 }
 
+displayUsage () {
+  echo "usage: iplocator action [options]"
+  echo "  action: register, identify, locate"
+  echo "  options:"
+  echo "    -k --key the key for the machine"
+  echo "    -s --secret the secret for the application"
+}
+
+if [ -z "$1" ]; then
+  displayUsage
+fi
+
+if [ "$1" != "register" ] && [ "$1" != "identify" ] && [ "$1" != "locate" ]; then
+  echo "here"
+fi
